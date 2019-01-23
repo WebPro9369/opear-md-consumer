54528c75cc9c5fb97b1794f07c56ba59f7edc8a9
swordEdge <robert.hillman11@gmail.com>
2019-01-23 07:44:57 -0500

    profile & all services screen prototype

---
+++ src/screens/all-services/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+	ContainerView
+} from './styles';
+
+class AllServicesScreen extends React.PureComponent {
+	render () {
+		return (
+			<ContainerView>
+			</ContainerView>
+		)
+	}
+};
+
+export default AllServicesScreen;
