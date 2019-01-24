661e909ffab1826374121bdedf68bd4d892f8b6f
swordEdge <robert.hillman11@gmail.com>
2019-01-24 16:32:52 -0500

    added sideBar scaffold

---
+++ src/screens/side-bar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+	ContainerView
+} from './styles';
+
+class SideBar extends React.PureComponent {
+	render () {
+		return (
+			<ContainerView>
+			</ContainerView>
+		)
+	}
+};
+
+export default SideBar;
