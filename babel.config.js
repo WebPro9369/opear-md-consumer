85a0703e35d0e3c8a88f581b2899a93ca7211507
Reo Liu <liudev555@gmail.com>
2019-02-12 21:45:44 +0800

    Babel and JS config added

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          "legacy": true
+        }
+      ], 
+      ["functional-hmr"]
+    ]
+  };
+};
