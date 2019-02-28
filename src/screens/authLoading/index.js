42995c23ccee28c995521f721c6a315d1688812e
Reo Liu <liudev555@gmail.com>
2019-02-28 09:11:31 +0800

    AuthLoading screen added.

---
+++ src/screens/authLoading/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { ActivityIndicator, View } from "react-native";
+
+class AuthLoadingScreen extends Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const userAuthenticated = false;
+    this.props.navigation.navigate(userAuthenticated ? "Dashboard" : "Onboarding");
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size="large" color="#7F5DB0" />
+      </View>
+    );
+  }
+}
+
+export default AuthLoadingScreen;
