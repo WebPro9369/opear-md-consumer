14cf47eb6573201963bc20203a65c3516248e074
Reo Liu <liudev555@gmail.com>
2019-02-28 17:35:43 +0800

    Updated Root container

--- src/root-container.js
+++ src/root-container.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import { AppNavigationContainer } from '@navigation/main.navigator';
-
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
-import { colors } from '@utils/constants';
+import React from "react";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components/native";
+import { AppNavigationContainer } from "./navigation/main.navigator";
+import { colors } from "./utils/constants";
 
 const Root = styled.View`
-	flex: 1;
-	background-color: ${ props => props.theme.WHITE };
+  flex: 1;
+  background-color: ${props => props.theme.WHITE};
 `;
 
 export default class RootContainer extends React.Component {
-	render() {
-		return (
-			<ThemeProvider theme={colors}>
-				<Root>
-					<StatusBar />
-					<AppNavigationContainer />
-				</Root>
-			</ThemeProvider>
-		);
-	}
-};
+  render() {
+    return (
+      <ThemeProvider theme={colors}>
+        <Root>
+          <StatusBar />
+          <AppNavigationContainer />
+        </Root>
+      </ThemeProvider>
+    );
+  }
+}
