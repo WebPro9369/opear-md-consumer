899be0a8e6acc71af2fcfe36c0536fc63b3547a5
Reo Liu <liudev555@gmail.com>
2019-03-05 00:10:24 +0800

    Added babel-eslint, fixed ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  extends: ["airbnb", "plugin:prettier/recommended"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
+      legacyDecorators: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
-  },
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+    "react/prop-types": ["error", { ignore: ["navigation"] }]
+  }
 };
