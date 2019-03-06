23dc241673952884f3e47e44c044ab225a48fe35
Reo Liu <liudev555@gmail.com>
2019-03-07 04:24:52 +0800

    New dashboard screen for select-date-time

---
+++ src/screens/dashboard/select-date-time/index.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { DateTime } from "luxon";
+import { StyledText } from "../../../components/text";
+import { NavHeader } from "../../../components/nav-header";
+import { ContainerView, View } from "../../../components/views";
+import { DateCircle, TimeSelector } from "./styles";
+import { colors } from "../../../utils/constants";
+
+class SelectDateTimeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    let luxonDate = DateTime.local();
+    const dates = [];
+    for (let i = 0; i < 10; i += 1) {
+      dates.push({
+        key: `${i + 1}`,
+        day: luxonDate.weekdayShort,
+        date: luxonDate.toFormat("LLL d"),
+        selected: false
+      });
+      luxonDate = luxonDate.plus({ days: 1 });
+    }
+    this.state = {
+      selectedDate: null,
+      selectedTime: null,
+      startHour: 8,
+      endHour: 18,
+      dates
+    };
+  }
+
+  render() {
+    const {
+      navigation: { goBack, navigate }
+    } = this.props;
+    const {
+      selectedDate,
+      selectedTime,
+      dates,
+      startHour,
+      endHour
+    } = this.state;
+    const hours = [];
+    for (let i = startHour; i <= endHour; i += 1) {
+      hours.push({ key: `${i}`, value: i });
+    }
+
+    return (
+      <ContainerView padding={0}>
+        <View
+          style={{
+            paddingLeft: 16,
+            paddingRight: 16,
+            paddingTop: 24,
+            paddingBottom: 6
+          }}
+        >
+          <NavHeader
+            title=""
+            size="small"
+            hasBackButton
+            onPressBackButton={() => goBack()}
+          />
+        </View>
+        <View>
+          <View style={{ paddingLeft: 16, paddingRight: 16 }}>
+            <StyledText
+              fontFamily="Flama-Medium"
+              fontSize={28}
+              color={colors.BLACK87}
+              lineHeight={30}
+            >
+              Select date and time
+            </StyledText>
+          </View>
+          <View
+            style={{
+              paddingTop: 32,
+              paddingBottom: 20,
+              paddingLeft: 16,
+              paddingRight: 16
+            }}
+          >
+            <FlatList
+              horizontal
+              data={dates}
+              renderItem={({ item }) => (
+                <DateCircle
+                  key={item.key}
+                  day={item.day}
+                  date={item.date}
+                  selected={item.selected}
+                  onPress={() => {
+                    const newDates = dates.map(date => {
+                      if (item.key === date.key) {
+                        this.setState({
+                          selectedDate: date.date
+                        });
+                        return {
+                          ...date,
+                          selected: true
+                        };
+                      }
+                      return {
+                        ...date,
+                        selected: false
+                      };
+                    });
+                    this.setState({ dates: newDates });
+                  }}
+                />
+              )}
+              style={{ padding: 12 }}
+            />
+          </View>
+          <View
+            style={{
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0
+            }}
+          >
+            <FlatList
+              data={hours}
+              renderItem={({ item }) => (
+                <TimeSelector
+                  key={item.key}
+                  start={item.value}
+                  end={item.value + 1}
+                  selected={item.value === selectedTime}
+                  onPress={() => {
+                    this.setState({
+                      selectedTime: item.value
+                    });
+                  }}
+                  onConfirm={() => navigate("BookingReview")}
+                />
+              )}
+              style={{ paddingLeft: 16, paddingRight: 16 }}
+            />
+          </View>
+        </View>
+      </ContainerView>
+    );
+  }
+}
+
+export default SelectDateTimeScreen;
---
+++ src/screens/dashboard/select-date-time/styles.js
@@ -0,0 +1,124 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components/native";
+import { colors } from "../../../utils/constants";
+import { StyledText } from "../../../components/text";
+
+export const ContentButton = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 24px;
+  padding-right: 24px;
+  margin-top: 6px;
+  border-style: solid;
+  border-width: ${props => (props.selected ? "2px" : "1px")};
+  border-color: ${props => (props.selected ? colors.BLUE : colors.MIDGREY)};
+  border-radius: 5px;
+`;
+
+export const DateCircleWrapper = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 72px;
+  height: 72px;
+  background-color: ${props => (props.selected ? colors.BLUE : colors.WHITE)};
+  color: ${props => (props.selected ? colors.WHITE : colors.BLACK)};
+  border-radius: 36px;
+  border-width: 0.5px;
+  border-color: ${colors.MIDGREY};
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const DateCircle = ({ day, date, selected, ...rest }) => (
+  <DateCircleWrapper selected={selected} {...rest}>
+    <StyledText
+      fontSize={14}
+      lineHeight={24}
+      fontFamily="Flama-Medium"
+      color={selected ? colors.WHITE : colors.BLACK87}
+    >
+      {day}
+    </StyledText>
+    <StyledText
+      fontSize={14}
+      lineHeight={24}
+      color={selected ? colors.WHITE : colors.BADGE_TEXT_GREY}
+    >
+      {date}
+    </StyledText>
+  </DateCircleWrapper>
+);
+
+DateCircle.propTypes = {
+  day: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired
+};
+
+export const TimeSelectorWrapper = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
+  background-color: ${props =>
+    props.selected ? colors.DARKSKYBLUE : colors.WHITE};
+  border-radius: 8px;
+  border-width: 0.5px;
+  border-color: ${colors.MIDGREY};
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-left: 28px;
+  padding-right: 28px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+`;
+
+export const ConfirmButton = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 93px;
+  height: 40px;
+  border-radius: 8px;
+  border-color: ${colors.WHITE};
+  border-width: 0.5px;
+`;
+
+export const TimeSelector = ({ start, end, selected, onConfirm, ...rest }) => (
+  <TimeSelectorWrapper selected={selected} {...rest}>
+    <StyledText
+      fontSize={16}
+      color={selected ? colors.WHITE : colors.BLACK87}
+      fontFamily={selected ? "Flama-Medium" : "Flama-Basic"}
+    >
+      {start >= 12 ? `${start - 12 || 12}pm` : `${start || 12}am`}
+      {" - "}
+      {end >= 12 ? `${end - 12 || 12}pm` : `${end || 12}am`}
+    </StyledText>
+    {selected ? (
+      <ConfirmButton onPress={onConfirm}>
+        <StyledText
+          fontSize={16}
+          fontFamily="Flama-Medium"
+          color={colors.WHITE}
+        >
+          CONFIRM
+        </StyledText>
+      </ConfirmButton>
+    ) : null}
+  </TimeSelectorWrapper>
+);
+
+TimeSelector.propTypes = {
+  start: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onConfirm: PropTypes.func.isRequired
+};
