123efa5ed3a8cf3bb59b6065ee9aa595256ee876
Reo Liu <liudev555@gmail.com>
2019-03-13 17:02:00 +0800

    Reactotron config: added mst

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from "reactotron-react-native";
+import { mst } from "reactotron-mst";
 import debugConfig from "./config/debug-config";
 
 if (debugConfig.useReactotron) {
   Reactotron.configure({ host: "localhost" })
     .useReactNative()
+    .use(mst())
     .connect();
 
   console.tron = Reactotron;
