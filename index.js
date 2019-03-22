b6d5b79a52b650e2e2f44097d727ad1fa685e826
Reo Liu <liudev555@gmail.com>
2019-03-23 02:07:15 +0800

    Disabled console warnings for presentation purpose

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 
+// Disable yellow warning boxes
+console.disableYellowBox = true;
+
 AppRegistry.registerComponent(appName, () => App);
