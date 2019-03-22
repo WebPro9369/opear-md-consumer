4c6d4795137be8e2bc5e57a73baa16079d4e0ae4
Reo Liu <liudev555@gmail.com>
2019-03-23 02:07:50 +0800

    Added new screen `ScanCard` in which users can scan their cards using camera

--- src/screens/account/scan-card/index.js
+++ src/screens/account/scan-card/index.js
@@ -1,27 +1,60 @@
 import React, { Component } from "react";
-import { CardIOView, CardIOUtilities } from "react-native-awesome-card-io";
-import { View } from "../../../components/views";
+import {
+  CardIOView,
+  CardIOModule,
+  CardIOUtilities
+} from "react-native-awesome-card-io";
+import { ContainerView, View, HeaderWrapper } from "../../../components/views";
+import { StyledText } from "../../../components/text";
+import { NavHeader } from "../../../components/nav-header";
 
 class ScanCardComponent extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   CardIOUtilities.preload();
-  // }
-
-  componentWillMount() {
+  constructor(props) {
+    super(props);
     CardIOUtilities.preload();
+    console.tron.log("CardIOUtilities preloaded");
   }
 
+  componentWillMount() {
+    // CardIOUtilities.preload();
+  }
+
+  scanCard = () => {
+    CardIOModule.scanCard()
+      .then(card => {
+        // the scanned card
+      })
+      .catch(() => {
+        // the user cancelled
+      });
+  };
+
   didScanCard = card => {
+    const {
+      navigation: { goBack }
+    } = this.props;
+
     // the scanned card
     console.tron.log("Card scanned: ", card);
+    goBack();
   };
 
   render() {
+    const {
+      navigation: { goBack }
+    } = this.props;
     return (
-      <View>
+      <ContainerView>
+        <HeaderWrapper>
+          <NavHeader
+            title="Scan your card"
+            size="medium"
+            hasBackButton
+            onPressBackButton={() => goBack()}
+          />
+        </HeaderWrapper>
         <CardIOView didScanCard={this.didScanCard} style={{ flex: 1 }} />
-      </View>
+      </ContainerView>
     );
   }
 }
