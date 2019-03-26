281ae05b72e4a18df658569c13cf51757256ecfc
Reo Liu <liudev555@gmail.com>
2019-03-26 23:50:56 +0800

    AddCardScreen connected with mobx store to pull card info

--- src/screens/account/add-card/index.js
+++ src/screens/account/add-card/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { inject, observer, PropTypes } from "mobx-react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { FormTextInput } from "../../../components/text";
 import { NavHeader } from "../../../components/nav-header";
@@ -14,55 +15,82 @@ import {
 } from "../../../components/views/keyboard-view";
 import { colors } from "../../../utils/constants";
 
-const AddCardScreen = ({ navigation: { goBack, navigate } }) => (
-  <KeyboardAvoidingView behavior="padding" enabled>
-    <NavHeader
-      title="Add Card"
-      size="medium"
-      onPressBackButton={() => goBack()}
-      hasBackButton
-    />
-    <FormWrapper>
-      <FormInputView>
-        <FormTextInput
-          label="Card Number"
-          placeholder="1234 5678 3456 2456"
-          leftIcon={
-            <FontAwesome name="cc-visa" size={30} color={colors.BLUE} />
-          }
-          rightIcon={
-            // eslint-disable-next-line react/jsx-wrap-multilines
-            <TouchableView onPress={() => navigate("PaymentScanCard")}>
-              <FontAwesome name="camera" size={30} color={colors.LIGHTGREEN} />
-            </TouchableView>
-          }
-        />
-      </FormInputView>
-      <FormInputView>
-        <FlexView>
+const AddCardScreen = ({
+  navigation: { goBack, navigate },
+  store: { userStore }
+}) => {
+  const {
+    cardInfo: { cardNumber, expiryYear, expiryMonth, cvv, fullName }
+  } = userStore;
+  return (
+    <KeyboardAvoidingView behavior="padding" enabled>
+      <NavHeader
+        title="Add Card"
+        size="medium"
+        onPressBackButton={() => goBack()}
+        hasBackButton
+      />
+      <FormWrapper>
+        <FormInputView>
           <FormTextInput
-            label="Exp. Date"
-            placeholder="## / ##"
-            style={{
-              width: 120,
-              marginRight: 40
-            }}
+            label="Card Number"
+            value={cardNumber}
+            placeholder="1234 5678 3456 2456"
+            leftIcon={
+              <FontAwesome name="cc-visa" size={30} color={colors.BLUE} />
+            }
+            rightIcon={
+              // eslint-disable-next-line react/jsx-wrap-multilines
+              <TouchableView onPress={() => navigate("PaymentScanCard")}>
+                <FontAwesome
+                  name="camera"
+                  size={30}
+                  color={colors.LIGHTGREEN}
+                />
+              </TouchableView>
+            }
           />
+        </FormInputView>
+        <FormInputView>
+          <FlexView>
+            <FormTextInput
+              label="Exp. Date"
+              value={
+                expiryYear && expiryMonth
+                  ? `${expiryYear} / ${expiryMonth}`
+                  : null
+              }
+              placeholder="## / ##"
+              style={{
+                width: 120,
+                marginRight: 40
+              }}
+            />
+            <FormTextInput
+              label="CVV"
+              value={cvv}
+              placeholder="###"
+              style={{
+                width: 120
+              }}
+            />
+          </FlexView>
+        </FormInputView>
+        <FormInputView>
           <FormTextInput
-            label="CVV"
-            placeholder="###"
-            style={{
-              width: 120
-            }}
+            label="Full Name"
+            value={fullName}
+            placeholder="Full Name"
           />
-        </FlexView>
-      </FormInputView>
-      <FormInputView>
-        <FormTextInput label="Full Name" placeholder="Full Name" />
-      </FormInputView>
-    </FormWrapper>
-    <ServiceButton title="Add Card" onPress={() => goBack()} />
-  </KeyboardAvoidingView>
-);
+        </FormInputView>
+      </FormWrapper>
+      <ServiceButton title="Add Card" onPress={() => goBack()} />
+    </KeyboardAvoidingView>
+  );
+};
 
-export default AddCardScreen;
+AddCardScreen.propTypes = {
+  store: PropTypes.observableObject.isRequired
+};
+
+export default inject("store")(observer(AddCardScreen));
