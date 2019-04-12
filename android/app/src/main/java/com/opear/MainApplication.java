7887a26ff05d133c90fa29967ecf359ba988ac4d
Reo Liu <liudev555@gmail.com>
2019-04-13 00:18:27 +0900

    Android settings updated

--- android/app/src/main/java/com/opear/MainApplication.java
+++ android/app/src/main/java/com/opear/MainApplication.java
@@ -3,6 +3,7 @@ package com.opear;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
