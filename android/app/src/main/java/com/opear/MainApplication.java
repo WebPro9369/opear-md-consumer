47225d4d281bf90b8a6ce64b7e9ef4e9ec7734a4
Reo Liu <liudev555@gmail.com>
2019-05-10 18:25:46 +0900

    Updated android and ios builds

--- android/app/src/main/java/com/opear/MainApplication.java
+++ android/app/src/main/java/com/opear/MainApplication.java
@@ -3,6 +3,7 @@ package com.opear;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfingerprint.FingerprintAuthPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FingerprintAuthPackage(),
             new ReactNativePushNotificationPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
