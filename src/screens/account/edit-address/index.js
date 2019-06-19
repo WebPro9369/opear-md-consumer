786661bc49123097a7fcaedd35ca972315aeaddd
Kyle Sloka-Frey <kyleslokafrey@Kyles-MacBook-Pro.local>
2019-06-19 14:43:24 -0400

    hook up edit address to the store
    Hooked in the edit address screen to use the user store
    patch the parent profile on update

--- src/screens/account/edit-address/index.js
+++ src/screens/account/edit-address/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { inject, observer, PropTypes } from "mobx-react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { FormTextInput } from "../../../components/text";
 import { NavHeader } from "../../../components/nav-header";
@@ -9,25 +10,84 @@ import {
   FormInputView
 } from "../../../components/views/keyboard-view";
 import { colors } from "../../../utils/constants";
+import { updateParent } from "@services/opear-api"
 
 const { LIGHTGREEN } = colors;
 
+@inject("store")
+@observer
 class EditAddressScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      address: "22341 Justice Avenue",
-      city: "San Francisco",
-      zip: "94043",
-      locationName: "Eddie's House"
+  static propTypes = {
+      store: PropTypes.observableObject.isRequired
     };
+
+    constructor(props) {
+      super(props);
+
+      const {
+        store: {
+          userStore: {
+            address: { name, street, city, zip_code }
+          }
+        }
+      } = props;
+
+      this.state = {
+        name,
+        street,
+        city,
+        zip_code
+      };
+
+      this.handleInputChange = this.handleInputChange.bind(this);
   }
+
+  handleInputChange = name => value => {
+    return this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmit = () => {
+    const {
+      navigation: { goBack },
+      store: {
+        userStore: { id, address }
+      }
+    } = this.props;
+
+    const { street, city, zip_code, name } = this.state;
+    const data = {
+      parent: {
+        address: [
+          {
+            name,
+            street,
+            city,
+            zip: zip_code
+          }
+        ]
+      }
+    };
+
+    const successHandler = () => {
+      address
+        .setName(name)
+        .setStreet(street)
+        .setCity(city)
+        .setZipCode(zip_code);
+
+      goBack();
+    };
+
+    updateParent(id, data, { successHandler });
+  };
 
   render() {
     const {
       navigation: { goBack }
     } = this.props;
-    const { address, city, zip, locationName } = this.state;
+    const { street, city, zip_code, name } = this.state;
     return (
       <KeyboardAvoidingView behavior="padding" enabled>
         <NavHeader
@@ -40,7 +100,8 @@ class EditAddressScreen extends React.Component {
           <FormInputView>
             <FormTextInput
               label="Address"
-              value={address}
+              value={street}
+              onChangeText={this.handleInputChange("street")}
               rightIcon={
                 <FontAwesome name="map-marker" size={30} color={LIGHTGREEN} />
               }
@@ -55,21 +116,24 @@ class EditAddressScreen extends React.Component {
                   marginRight: 40
                 }}
                 value={city}
+                onChangeText={this.handleInputChange("city")}
               />
               <FormTextInput
                 label="Zip"
                 style={{
                   width: 120
                 }}
-                value={zip}
+                value={zip_code}
+                onChangeText={this.handleInputChange("zip_code")}
               />
             </FlexView>
           </FormInputView>
           <FormInputView>
-            <FormTextInput label="Location Name" value={locationName} />
+            <FormTextInput label="Location Name" value={name}
+            onChangeText={this.handleInputChange("city")} />
           </FormInputView>
         </FormWrapper>
-        <ServiceButton title="Update Address" onPress={() => goBack()} />
+        <ServiceButton title="Update Address" onPress={this.onSubmit} />
       </KeyboardAvoidingView>
     );
   }
