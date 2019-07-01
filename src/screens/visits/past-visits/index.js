9893f390e66d1dbf111e7cfcea9e8dc3b5a04a93
Kyle Sloka-Frey <kyleslokafrey@Kyles-MacBook-Pro.local>
2019-07-01 16:20:57 -0400

    Added minutes to visit times on upcoming/past screens

--- src/screens/visits/past-visits/index.js
+++ src/screens/visits/past-visits/index.js
@@ -130,7 +130,7 @@ class PastVisitsScreen extends React.Component {
     const visitsDisplayStack = [];
     const addedTimes = [];
     const dayOptions = { month: "long", day: "numeric" };
-    const timeOptions = { day: undefined, hour: "numeric" };
+    const timeOptions = { day: undefined, hour: "numeric", minute: "2-digit" };
 
     visits.map(visit => {
       const { appointmentTime } = visit;
