2e7747108b2f59d45bab8c011d9a1f3205d98d0f
Reo Liu <liudev555@gmail.com>
2019-07-17 18:15:09 +0900

    Refactored InactiveUserBanner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,23 +3,14 @@ import PropTypes from "prop-types";
 import { StyledText } from "../text";
 import { MatchingMessageWrapper } from "./styles";
 
-class InactiveUserBanner extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { userIsActive, fontSize, lineHeight, ...rest } = this.props;
-
-    return !userIsActive ? (
-      <MatchingMessageWrapper>
-        <StyledText fontSize={fontSize} lineHeight={lineHeight}>
-          Opear is not yet available in your area - check back soon!
-        </StyledText>
-      </MatchingMessageWrapper>
-    ) : null;
-  }
-}
+const InactiveUserBanner = ({ userIsActive, fontSize, lineHeight }) =>
+  !userIsActive ? (
+    <MatchingMessageWrapper>
+      <StyledText fontSize={fontSize} lineHeight={lineHeight}>
+        Opear is not yet available in your area - check back soon!
+      </StyledText>
+    </MatchingMessageWrapper>
+  ) : null;
 
 InactiveUserBanner.propTypes = {
   userIsActive: PropTypes.bool,
